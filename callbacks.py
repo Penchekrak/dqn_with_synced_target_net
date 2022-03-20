27d678390c70f65a16351c80d5a3305a262f2f3f
Penchekrak <spiand97@gmail.com>
2022-03-20 22:26:17 +0300

    gcc callback

--- callbacks.py
+++ callbacks.py
@@ -12,7 +12,7 @@ class LogReplayVideoCallback(Callback):
     def __init__(self, path: str = 'video.mp4'):
         self.path = path
 
-    def on_train_epoch_end(self, trainer: "pl.Trainer", pl_module: DQN) -> None:
+    def on_train_epoch_end(self, trainer: "pl.Trainer", pl_module: DQN, **kwargs) -> None:
         with RecordVideo(pl_module.test_env, video_folder=self.path) as recorded_env:
             _ = pl_module.run_n_episodes(recorded_env, 1, 0.0)
         video_paths = [s for s in Path(self.path).iterdir() if s.suffix == '.mp4']
@@ -24,6 +24,6 @@ class GC(Callback):
     def __init__(self, clean_every_n_epochs: int = 1):
         self.clean_every_n_epochs = clean_every_n_epochs
 
-    def on_train_epoch_end(self, trainer: "pl.Trainer", pl_module: DQN) -> None:
+    def on_train_epoch_end(self, trainer: "pl.Trainer", pl_module: DQN, **kwargs) -> None:
         if trainer.current_epoch + 1 % self.clean_every_n_epochs == 0:
             gc.collect()

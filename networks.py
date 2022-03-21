940e525a94fe89c8b065c57f34e9996a3e23eba2
Nikolay Sheyko <nickolay.sheyko@gmail.com>
2022-03-21 19:05:09 +0300

    add LargeCNN

--- networks.py
+++ networks.py
@@ -33,6 +33,40 @@ class MediumCNN(SmallCNN):
             nn.Linear(512, n_actions)
         )
 
+class LargeCNN(SmallCNN):
+    def __init__(self, input_shape: Tuple[int], n_actions: int):
+        """
+        Args:
+            input_shape: observation shape of the environment
+            n_actions: number of discrete actions available in the environment
+        """
+        super(SmallCNN, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(),
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.head = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            # nn.BatchNorm1d(512),
+            nn.LeakyReLU(),
+            nn.Linear(512, n_actions)
+        )
+
 
 SmallMLP = partial(MLP, hidden_size=128)
 MediumMLP = partial(MLP, hidden_size=256)
